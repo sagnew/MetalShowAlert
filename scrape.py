578a9699df95c60f7ce7b1b180785c4f5189b0e5
Sam Agnew <sagnew@eden.rutgers.edu>
2015-05-29 01:58:26 -0400

    Refactor code and add doc strings

--- scrape.py
+++ scrape.py
@@ -5,6 +5,31 @@ from bs4 import BeautifulSoup
 
 
 def scrape_NYC_metal_scene():
+    '''
+    Returns an array of dictionaries representing each upcoming metal show
+    from nycmetalscene.com.
+
+    The dictionaries are structured as:
+        {
+            'original_date_text': The date text scraped from the website.
+            'show_date': The arrow object representing the date of the show.
+            'information': The text containing bands and venue information.
+        }
+    '''
+
+    def get_date_text(scraped_text):
+        '''
+        scraped_text: The raw scraped text
+                      representing the date of a show.
+
+        Returns a formatted version of the text for arrow to parse.
+        '''
+
+        # Accounts for some dates that have a "." and for multi-day events.
+        # Also makes September events compatible with arrow.
+        return re.sub(r"(st|nd|rd|th),", ",", scraped_text) \
+            .replace('.', '').split('&')[0].replace('Sept', 'Sep')
+
     url = 'http://nycmetalscene.com/'
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text)
@@ -16,16 +41,17 @@ def scrape_NYC_metal_scene():
         show_split = show.split(':')
         original_date_text, show_text = show_split[0], ':'.join(show_split[1:])
 
-        # Accounts for some dates that have a "." and for multi-day events.
-        # Also makes September events compatible with arrow.
-        date_text = re.sub(r"(st|nd|rd|th),", ",", original_date_text) \
-            .replace('.', '').split('&')[0].replace('Sept', 'Sep')
+        date_text = get_date_text(original_date_text)
 
         try:
             # Check for abbreviated months.
             show_date = arrow.get(date_text, 'ddd MMM D, YYYY')
 
-            shows.append((show_date, original_date_text, show_text))
+            shows.append({
+                'original_date_text': original_date_text,
+                'show_date': show_date,
+                'information': show_text
+            })
             continue
         except arrow.parser.ParserError:
             pass
@@ -33,7 +59,11 @@ def scrape_NYC_metal_scene():
             # Check for unabbreviated months.
             show_date = arrow.get(date_text, 'ddd MMMM D, YYYY')
 
-            shows.append((show_date, original_date_text, show_text))
+            shows.append({
+                'original_date_text': original_date_text,
+                'show_date': show_date,
+                'information': show_text
+            })
             continue
         except arrow.parser.ParserError:
             pass
@@ -41,7 +71,11 @@ def scrape_NYC_metal_scene():
             # Check for missing year.
             show_date = arrow.get(date_text, 'ddd MMM D')
 
-            shows.append((show_date, original_date_text, show_text))
+            shows.append({
+                'original_date_text': original_date_text,
+                'show_date': show_date,
+                'information': show_text
+            })
             continue
         except arrow.parser.ParserError:
             pass
@@ -49,7 +83,11 @@ def scrape_NYC_metal_scene():
             # Check for missing year, and unabbreviated month.
             show_date = arrow.get(date_text, 'ddd MMMM D')
 
-            shows.append((show_date, original_date_text, show_text))
+            shows.append({
+                'original_date_text': original_date_text,
+                'show_date': show_date,
+                'information': show_text
+            })
             continue
         except arrow.parser.ParserError:
             # This means that our parser caught something that wasn't a show
@@ -59,4 +97,7 @@ def scrape_NYC_metal_scene():
 
     return shows
 
-print len(scrape_NYC_metal_scene())
+for show in scrape_NYC_metal_scene():
+    print show['original_date_text']
+    print show['information']
+    print '\n'
