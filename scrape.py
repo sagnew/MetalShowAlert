f30e605fa966dcaf99930f92f08841baaaf087fa
Sam Agnew <sagnew@eden.rutgers.edu>
2015-05-29 23:37:37 -0400

    Remove some redundant code

--- scrape.py
+++ scrape.py
@@ -35,23 +35,24 @@ def scrape_NYC_metal_scene():
     soup = BeautifulSoup(html_text)
     shows_text = [show.text for show in
                   soup.findAll('td', id=re.compile('^Text2'))]
+
     shows = []
-    print len(shows_text[2:])
     for show in shows_text[2:]:
         show_split = show.split(':')
         original_date_text, show_text = show_split[0], ':'.join(show_split[1:])
 
         date_text = get_date_text(original_date_text)
 
+        show_dict = {
+            'original_date_text': original_date_text,
+            'information': show_text
+        }
         try:
             # Check for abbreviated months.
             show_date = arrow.get(date_text, 'ddd MMM D, YYYY')
 
-            shows.append({
-                'original_date_text': original_date_text,
-                'show_date': show_date,
-                'information': show_text
-            })
+            show_dict['show_date'] = show_date
+            shows.append(show_dict)
             continue
         except arrow.parser.ParserError:
             pass
@@ -59,11 +60,8 @@ def scrape_NYC_metal_scene():
             # Check for unabbreviated months.
             show_date = arrow.get(date_text, 'ddd MMMM D, YYYY')
 
-            shows.append({
-                'original_date_text': original_date_text,
-                'show_date': show_date,
-                'information': show_text
-            })
+            show_dict['show_date'] = show_date
+            shows.append(show_dict)
             continue
         except arrow.parser.ParserError:
             pass
@@ -71,11 +69,8 @@ def scrape_NYC_metal_scene():
             # Check for missing year.
             show_date = arrow.get(date_text, 'ddd MMM D')
 
-            shows.append({
-                'original_date_text': original_date_text,
-                'show_date': show_date,
-                'information': show_text
-            })
+            show_dict['show_date'] = show_date
+            shows.append(show_dict)
             continue
         except arrow.parser.ParserError:
             pass
@@ -83,11 +78,8 @@ def scrape_NYC_metal_scene():
             # Check for missing year, and unabbreviated month.
             show_date = arrow.get(date_text, 'ddd MMMM D')
 
-            shows.append({
-                'original_date_text': original_date_text,
-                'show_date': show_date,
-                'information': show_text
-            })
+            show_dict['show_date'] = show_date
+            shows.append(show_dict)
             continue
         except arrow.parser.ParserError:
             # This means that our parser caught something that wasn't a show
